8e0462aec83d70a60322dea5a01de8bf8bdc4f84
df232323 <aleksa_uvarova@inbox.ru>
2026-05-08 19:54:43 +0300

    Update server.py

--- server.py
+++ server.py
@@ -1,53 +1,63 @@
-import random
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
-from aiogram import Bot
-
-from config import (
-    BOT_TOKEN,
-    GROUP_ID
-)
+import random
+import requests
+import os
 
 app = FastAPI()
-
-bot = Bot(token=BOT_TOKEN)
 
 templates = Jinja2Templates(directory="templates")
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
 
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+GROUP_ID = os.getenv("GROUP_ID")
+MANAGER_USERNAME = os.getenv("MANAGER_USERNAME")
+
 
 @app.get("/", response_class=HTMLResponse)
-async def index(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse(
         "index.html",
-        {"request": request}
+        {
+            "request": request,
+            "manager_username": MANAGER_USERNAME
+        }
     )
 
 
 @app.post("/submit")
 async def submit(request: Request):
+
     data = await request.json()
 
     track_code = random.randint(1000, 9999)
 
-    text = f'''
+    text = f"""
 🆕 Новая заявка Ozon
 
-👤 Имя: {data['name']}
-🎂 Возраст: {data['age']}
-📱 Телефон: {data['phone']}
-💬 Telegram: {data['telegram']}
-🧠 Опыт: {data['experience']}
-⏰ Готов уделять: {data['hours']}
+👤 ФИО: {data.get('fio')}
+🎂 Дата рождения: {data.get('birth')}
+📱 Телефон: {data.get('phone')}
+✈ Telegram: {data.get('telegram')}
+
+💼 Опыт: {data.get('experience')}
+⏰ Время: {data.get('time')}
+📲 Смартфон: {data.get('device')}
 
 🆔 HR-код: {track_code}
-'''
+"""
 
-    await bot.send_message(GROUP_ID, text)
+    requests.post(
+        f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage",
+        json={
+            "chat_id": GROUP_ID,
+            "text": text
+        }
+    )
 
     return JSONResponse({
         "success": True,
